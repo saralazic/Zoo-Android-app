5d9bfc0eac22d6f1f32194ceefe9066aea4818ee
saralazicc <sara.lazic@ctd.ai>
2023-05-31 22:57:57 +0200

    Added about page

--- app/src/main/java/com/example/pandicazoovrt/AboutActivity.java
+++ app/src/main/java/com/example/pandicazoovrt/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.example.pandicazoovrt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +17,50 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         Log.d("AboutActivity", "routed to About");
+
+        ImageButton ticketsImgBtn = findViewById(R.id.ticketsIconButton);
+        ticketsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutActivity.this, TicketsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton eventsImgBtn = findViewById(R.id.eventsIconButton);
+        eventsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutActivity.this, EventsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton animalsImgBtn = findViewById(R.id.animalsIconButton);
+        animalsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutActivity.this, AnimalsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton notificationsImgBtn = findViewById(R.id.notificationsIconButton);
+        notificationsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutActivity.this, NotificationsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton accountImgBtn = findViewById(R.id.accountIconButton);
+        accountImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutActivity.this, AccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
