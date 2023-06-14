796ed83cf39809a24b6736443550a3e3d6ccc77a
saralazicc <sara.lazic@ctd.ai>
2023-06-14 22:26:24 +0200

    adult tickets wip

--- app/src/main/java/com/example/pandicazoovrt/tickets/AdultsTicketsActivity.java
+++ app/src/main/java/com/example/pandicazoovrt/tickets/AdultsTicketsActivity.java
@@ -3,6 +3,7 @@ package com.example.pandicazoovrt.tickets;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,16 +11,56 @@ import com.example.pandicazoovrt.R;
 
 public class AdultsTicketsActivity extends AppCompatActivity {
 
+    private NumberPicker picker1, picker2, picker3, picker4;
+    private int cnt1, cnt2, cnt3, cnt4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tickets_adults);
 
-        Button childrenBtn = findViewById(R.id.ChildrenBtn);
-        childrenBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
+        picker1 = findViewById(R.id.numberpicker_1);
+        picker1.setMaxValue(100);
+        picker1.setMinValue(0);
+
+        picker1.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                cnt1 = picker1.getValue();
+            }
+        });
+
+        picker2 = findViewById(R.id.numberpicker_2);
+        picker2.setMaxValue(100);
+        picker2.setMinValue(0);
+
+        picker2.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                int cnt2 = picker2.getValue();
+            }
+        });
+
+        picker3 = findViewById(R.id.numberpicker_3);
+        picker3.setMaxValue(100);
+        picker3.setMinValue(0);
+
+        picker3.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                int cnt3 = picker3.getValue();
+            }
+        });
+
+        picker4 = findViewById(R.id.numberpicker_4);
+        picker4.setMaxValue(100);
+        picker4.setMinValue(0);
+
+        picker4.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                int cnt4 = picker4.getValue();
             }
         });
 
