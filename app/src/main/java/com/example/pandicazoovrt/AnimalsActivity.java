4f61373e636c6067f07d3cf0267efe9413415b2b
saralazicc <sara.lazic@ctd.ai>
2023-06-16 22:14:09 +0200

    started scrollable events

--- app/src/main/java/com/example/pandicazoovrt/AnimalsActivity.java
+++ app/src/main/java/com/example/pandicazoovrt/AnimalsActivity.java
@@ -1,16 +1,66 @@
 package com.example.pandicazoovrt;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.pandicazoovrt.tickets.TicketsActivity;
 
 public class AnimalsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animals);
 
-        Log.d("AboutActivity", "routed to Animals");
+        ImageButton ticketsImgBtn = findViewById(R.id.ticketsIconButton);
+        ticketsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnimalsActivity.this, TicketsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton eventsImgBtn = findViewById(R.id.eventsIconButton);
+        eventsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnimalsActivity.this, EventsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton animalsImgBtn = findViewById(R.id.animalsIconButton);
+        animalsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnimalsActivity.this, AnimalsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton notificationsImgBtn = findViewById(R.id.notificationsIconButton);
+        notificationsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnimalsActivity.this, NotificationsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton accountImgBtn = findViewById(R.id.accountIconButton);
+        accountImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnimalsActivity.this, AccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/pandicazoovrt/EventsActivity.java
+++ app/src/main/java/com/example/pandicazoovrt/EventsActivity.java
@@ -1,9 +1,14 @@
 package com.example.pandicazoovrt;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
+import com.example.pandicazoovrt.tickets.TicketsActivity;
 
 
 public class EventsActivity extends AppCompatActivity {
@@ -12,6 +17,50 @@ public class EventsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
 
-        Log.d("AboutActivity", "routed to Events");
+        ImageButton ticketsImgBtn = findViewById(R.id.ticketsIconButton);
+        ticketsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EventsActivity.this, TicketsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton eventsImgBtn = findViewById(R.id.eventsIconButton);
+        eventsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EventsActivity.this, EventsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton animalsImgBtn = findViewById(R.id.animalsIconButton);
+        animalsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EventsActivity.this, AnimalsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton notificationsImgBtn = findViewById(R.id.notificationsIconButton);
+        notificationsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EventsActivity.this, NotificationsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton accountImgBtn = findViewById(R.id.accountIconButton);
+        accountImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EventsActivity.this, AccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
